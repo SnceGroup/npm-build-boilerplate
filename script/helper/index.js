bcdfc7a71e0298406abb141736319cb75f6f8e2c
Alberto Terragni <alberto.terragni@sncegroup.ch>
2017-02-22 09:37:42 +0100

    Add helper for getting root build folder

---
+++ script/helper/index.js
@@ -0,0 +1,16 @@
+/**
+ * Get the root build folder by checking the NODE_ENV variable
+ *
+ * @returns {String} The root folder for the build
+ */
+var getBuildRootFolder = function() {
+  if(process.env.NODE_ENV && process.env.NODE_ENV === 'prod') {
+    return './build-dist';
+  }
+  
+  return './build-dev';
+};
+
+module.exports = {
+  getBuildRootFolder: getBuildRootFolder,
+}
