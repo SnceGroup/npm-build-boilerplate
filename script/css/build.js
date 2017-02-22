3db77cf352768c5b97e86bc4f1c4fd035e8c10d6
Alberto Terragni <alberto.terragni@sncegroup.ch>
2017-02-22 11:48:03 +0100

    Add clean-css-cli script

--- script/css/build.js
+++ script/css/build.js
@@ -2,4 +2,9 @@ var isProduction = require('../helper').isProductionBuild();
 var execCommand = require('../helper/exec-command');
 
 var command = 'npm run scss && npm run autoprefixer';
+
+if(isProduction) {
+  command = command + ' && npm run clean-css';
+}
+
 execCommand(command);
