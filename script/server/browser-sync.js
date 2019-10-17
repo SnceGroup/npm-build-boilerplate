96a6fbe7fe05159534228c1d48f9f6e631f758f4
Alberto Terragni <alberto.terragni@sncegroup.ch>
2019-10-17 10:00:42 +0200

    Add https

--- script/server/browser-sync.js
+++ script/server/browser-sync.js
@@ -1,5 +1,5 @@
-var buildFolder = require('../helper').getBuildRootFolder();
-var execCommand = require('../helper/exec-command');
+const buildFolder = require('../helper').getBuildRootFolder();
+const execCommand = require('../helper/exec-command');
 
-var command = 'browser-sync start --server --ss "'+ buildFolder +'" --files "'+ buildFolder +'/css/*.css, '+ buildFolder +'/js/*.js, '+ buildFolder +'/*.html"';
+const command = `browser-sync start --https --server --ss "${buildFolder}" --files "${buildFolder}/css/*.css, ${buildFolder}/js/*.js, ${buildFolder}/*.html"`;
 execCommand(command);
