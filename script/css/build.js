ec7b79b2c9c48dd662ab3b998fab63193ed14099
Alberto Terragni <alberto.terragni@sncegroup.ch>
2019-10-17 09:45:42 +0200

    Add prettier

--- script/css/build.js
+++ script/css/build.js
@@ -6,7 +6,7 @@ const execCommand = require('../helper/exec-command');
 let command = `postcss 'src/css/*.css' --dir '${buildFolder}/css'`;
 
 if(isProduction) {
-  command = `${command} && npm run clean-css`;
+  command = `${command} && npm run clean-css && npm run format:css`;
 }
 
 execCommand(command);
